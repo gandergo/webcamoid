28f3950d09d05b0e02972eb60a891b5f6a0038c1
gandergo <emailou@gmail.com>
2022-11-09 15:35:32 +0800

    build: add gst-plugins-bad

--- ports/ci/mac/install_deps.sh
+++ ports/ci/mac/install_deps.sh
@@ -46,6 +46,7 @@ if [ "${UPLOAD}" != 1 ]; then
         gstreamer \
         gst-plugins-base \
         gst-plugins-good \
+        gst-plugins-bad \
         pulseaudio \
         jack \
         libuvc
